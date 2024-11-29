f059eed7ce261773f826e9313dd2a84af571146b
savchenko-sergey2024 <savrostovdasha@gmail.com>
2024-11-29 15:31:27 +0300

    Загружаю урок 5 на проверку

---
+++ les5_game.py
@@ -0,0 +1,116 @@
+import file_operations
+
+from faker import Faker
+
+import random
+
+context1 = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд',
+]
+alphabet = {
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def main():
+    for cards in range(10):
+        fake = Faker("ru_RU")
+        context2 = (random.sample(context1, 3))
+
+        context2_list = ', '.join(context2)
+
+        for key, values in alphabet.items():
+            context2_list = context2_list.replace(key, values)
+
+        context2_list = context2_list.split(',')
+        context = {
+            'first_name': fake.first_name_male(),
+            'last_name': fake.last_name_male(),
+            'job': fake.job(),
+            'town': fake.city(),
+            'strength': random.randint(3, 18),
+            'agility': random.randint(3, 18),
+            'endurance': random.randint(3, 18),
+            'intelligence': random.randint(3, 18),
+            'luck': random.randint(3, 18),
+            'skill_1': context2_list[0],
+            'skill_2': context2_list[1],
+            'skill_3': context2_list[2]}
+        file_operations.render_template('charsheet.svg', 'svg/resultfake-{0}.svg'.format(cards), context)
+
+
+if __name__ == '__main__':
+    main()
